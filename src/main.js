a98a32ef397f59b3fc33df72bbb01fb82d99ee5a
lui <luisnavarrodelangel@gmail.com>
2024-11-27 18:18:11 -0500

    First commit - homepage menu

--- src/main.js
+++ src/main.js
@@ -2,3 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+try {
+  createApp(App).mount('#app')
+} catch (error) {
+  console.error('Failed to mount Vue app:', error)
+}
